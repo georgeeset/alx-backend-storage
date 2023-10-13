ea55733b536329fbaf45e0f71e6c6ed0c7920bf9
georgeeset <georgeperfect4u@gmail.com>
2023-10-13 01:20:49 +0100

    tried using stored procedure

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,14 +1,14 @@
 -- SQL script that creates a trigger that decreases the
 -- quantity of an item after adding a new order.
 
-SELECT * FROM items;
-SELECT * FROM orders;
-
-INSERT INTO orders (item_name, number) VALUES ('apple', 1);
-INSERT INTO orders (item_name, number) VALUES ('apple', 3);
-INSERT INTO orders (item_name, number) VALUES ('pear', 2);
-
-SELECT "--";
-
-SELECT * FROM items;
-SELECT * FROM orders;
+DROP TRIGGER IF EXISTS reduce_quantity();
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
